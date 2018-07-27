560f2039e6796f18a2e2d2b9e4d695180d747a47
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-27 08:29:57 +0300

    Made OrderSummary component as statefull

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={ igKey }>
-          <span style={ {textTransform: 'capitalize'} }>{ igKey }</span>: { props.ingredients[igKey] }
+          <span style={ {textTransform: 'capitalize'} }>{ igKey }</span>: { this.props.ingredients[igKey] }
         </li>
       );
     });
 
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        { ingredientSummary }
-      </ul>
-      <p><strong>Total Price: { props.price.toFixed(2) }</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button
-        btnType="Danger"
-        clicked={ props.purchaseCanceled }>CANCEL</Button>
-      <Button
-        btnType="Success"
-        clicked={ props.purchaseContinued }>CONTINUE</Button>
-    </Auxiliary>
-  );
-};
+  render() {
+    return (
+      <Auxiliary>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          { this.ingredientSummary }
+        </ul>
+        <p><strong>Total Price: { this.props.price.toFixed(2) }</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button
+          btnType="Danger"
+          clicked={ this.props.purchaseCanceled }>CANCEL</Button>
+        <Button
+          btnType="Success"
+          clicked={ this.props.purchaseContinued }>CONTINUE</Button>
+      </Auxiliary>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
