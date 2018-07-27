89b0bf77c50293159a998cf5157628ee89157dc5
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-07-27 14:35:21 +0300

    Ejecting request and response interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,13 +10,18 @@ const withErrorHandler = (WrappedComponent, axios) => {
     };
 
     componentWillMount() {
-      axios.interceptors.request.use(reqest => {
+      this.requestInterceptor = axios.interceptors.request.use(reqest => {
         this.setState({error: null});
         return reqest;
       });
-      axios.interceptors.response.use(response => response, error => {
+      this.responseInterceptor = axios.interceptors.response.use(response => response, error => {
         this.setState({error: error});
       });
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responseInterceptor);
     }
 
     errorConfirmedHandler = () => {
