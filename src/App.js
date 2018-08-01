0f8203925ac9457cb591e342ef6d54920be0928f
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-08-01 08:34:54 +0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -8,16 +8,14 @@ import Checkout from './containers/Checkout/Checkout';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Layout>
-            <Switch>
-              <Route path="/" exact component={ BurgerBuilder }/>
-              <Route path="/checkout" component={ Checkout }/>
-            </Switch>
-          </Layout>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={ BurgerBuilder } />
+            <Route path="/checkout" component={ Checkout } />
+          </Switch>
+        </Layout>
+      </div>
     );
   }
 }
