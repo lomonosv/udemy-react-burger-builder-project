9f21c2eb497a668ff940ac8ac17f1e8d91ed2c8a
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-08-01 07:47:56 +0300

    Checkout component and router packages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder/>
-        </Layout>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Layout>
+            <Switch>
+              <Route path="/" exact component={ BurgerBuilder }/>
+              <Route path="/checkout" component={ Checkout }/>
+            </Switch>
+          </Layout>
+        </div>
+      </BrowserRouter>
     );
   }
 }
