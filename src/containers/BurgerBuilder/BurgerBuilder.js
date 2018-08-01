dcb039dbfce30cc102bdd05471d5a5330a296098
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-08-01 10:50:55 +0300

    Order submission

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,35 +90,12 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    // this.setState({loading: true});
-    //
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Vladimir',
-    //     address: {
-    //       street: 'Teststreet 1',
-    //       zipCode: '41351',
-    //       country: 'Ukraine'
-    //     },
-    //     email: 'test@test.com'
-    //   },
-    //   deliveryMethod: 'fastest'
-    // };
-    //
-    // axios.post('/orders', order)
-    //   .then(response => {
-    //     this.setState({loading: false, purchasing: false});
-    //   })
-    //   .catch(error => {
-    //     this.setState({loading: false, purchasing: false});
-    //   });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    }
 
-      const queryParams = [];
-      for (let i in this.state.ingredients) {
-          queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-      }
+    queryParams.push('price=' + this.state.totalPrice);
 
     this.props.history.push({
       pathname: '/checkout',
