45f57ff17793393c5ffa8af8ee6ee4ba7bc8d76b
Vladimir Lomonos <lomonosv.primary@gmail.com>
2018-08-16 10:50:01 +0300

    Logout manually

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Auxiliary from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
@@ -19,8 +20,11 @@ class Layout extends Component {
   render() {
     return (
       <Auxiliary>
-        <Toolbar drawerToggleClicked={ this.sideDrawerToggledHandler }/>
+        <Toolbar
+          isAuth={ this.props.isAuthenticated }
+          drawerToggleClicked={ this.sideDrawerToggledHandler }/>
         <SideDrawer
+          isAuth={ this.props.isAuthenticated }
           open={ this.state.showSideDrawer }
           closed={ this.sideDrawerToggledHandler }/>
         <main className={ classes.Content }>
@@ -31,4 +35,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
