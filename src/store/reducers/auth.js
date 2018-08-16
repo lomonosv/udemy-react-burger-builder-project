6f8927b919c4fcd11683ae4da3439056923ef7e6
Vladimir Lomonos <lomonosv.primary@gmail.com>
2018-08-16 09:08:05 +0300

    Logging users out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -31,11 +31,19 @@ const authFail = (state, action) => {
   });
 };
 
+const authLogout = (state, action) => {
+  return updateObject(state, {
+    token: null,
+    userId: null
+  });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionType.AUTH_START: return authStart(state, action);
     case actionType.AUTH_SUCCESS: return authSuccess(state, action);
     case actionType.AUTH_FAIL: return authFail(state, action);
+    case actionType.AUTH_LOGOUT: return authLogout(state, action);
     default: return state;
   }
 };
